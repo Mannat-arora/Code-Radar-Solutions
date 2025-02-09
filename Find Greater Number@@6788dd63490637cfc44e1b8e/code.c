3ce16473acd0e950137f35b4273edcf4115ae15b
MANNAT <192688920+Mannat-arora@users.noreply.github.com>
2025-02-09 09:15:10 +0530

    Solution to Find Greater Number

--- Find Greater Number@@6788dd63490637cfc44e1b8e/code.c
+++ Find Greater Number@@6788dd63490637cfc44e1b8e/code.c
@@ -3,9 +3,9 @@
 int main() {
     int a,b;
     scanf("%d %d", &a, &b);
-    if(a>b){
-        printf("%d", a);
-    } else{
+    if(a<b){
         printf("%d", b);
+    } else{
+        printf("%d", a);
     }
 }
